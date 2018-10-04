505d262d232e5fbae9e120d5409ea52a72267858
walter5746 <walter5746@gmail.com>
2018-10-04 16:42:49 -0500

    Testing file added Began __repr__ method

--- testing.py
+++ testing.py
@@ -7,10 +7,6 @@ class Roster():
         self.status = status
         self.atick = atick
 
-    def __repr__(self):
-        for i in r_list:
-            return str(i)
-
     def roster_entry():
         r_list = []
         for i in source_list:
@@ -23,20 +19,22 @@ class Roster():
         return r_list
 
 
-                 #        post, uid, zone, status, atick
-                 p1 = Roster(1, None, None, 0, None)
-    p2 = Roster(2, None, 2, 1, None)
-    p3 = Roster(3, None, 3, 1, None)
-    p4 = Roster(4, None, 4, 1, None)
-    p5 = Roster(5, None, 5, 5, None)
-    p6 = Roster(6, None, 6, 5, None)
-    p7 = Roster(7, None, 7, 5, None)
-    p8 = Roster(8, None, 8, 5, None)
-    p9 = Roster(9, None, 9, 1, None)
-    p10 = Roster(10, None, None, 1, None)
-    p11 = Roster(11, None, 11, 5, None)
-    p12 = Roster(12, None, 12, 5, None)
+#        post, uid, zone, status, atick
+p1 = Roster(1, None, None, 0, None)
+p2 = Roster(2, None, 2, 1, None)
+p3 = Roster(3, None, 3, 1, None)
+p4 = Roster(4, None, 4, 1, None)
+p5 = Roster(5, None, 5, 5, None)
+p6 = Roster(6, None, 6, 5, None)
+p7 = Roster(7, None, 7, 5, None)
+p8 = Roster(8, None, 8, 5, None)
+p9 = Roster(9, None, 9, 1, None)
+p10 = Roster(10, None, None, 1, None)
+p11 = Roster(11, None, 11, 5, None)
+p12 = Roster(12, None, 12, 5, None)
 
-    source_list = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12]
+source_list = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12]
 
-    Roster.roster_entry()
+Roster.roster_entry()
+
+print(source_list)
