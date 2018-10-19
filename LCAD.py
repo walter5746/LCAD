dcf9a7ffe2d54177da56a471e5afd12936d674e5
Walter Disney <walter5746@gmail.com>
2018-10-19 17:33:53 -0500

    Updated CallTypes and Roster Classes

--- LCAD.py
+++ LCAD.py
@@ -1,7 +1,4 @@
 """To do:
--Roster class
-    -Print roster method in Roster class
--Calltypes class
 -Ticket class
     -location parser method in Ticket class
     -make all input .upper()
@@ -13,21 +10,14 @@
 class Roster():
     """
     uid = unit identifier, atick = what ticket they are currently assigned to.
-    status: 0 = Unavailable, 1 = Stationary, 2 = One high priority call 3 = Break, 4 = On low priority call,
-    5 = Available.
-
-
+    status: 0 = Unavailable, 1 = Stationary, 2 = One high priority call 3 = Break, 4 = On low priority call, 5 = Available.
     """
     def __init__(self, post, uid, zone, status, atick):
         self.post = post
         self.uid = uid
         self.zone = zone
         self.status = status
-        self. atick = atick
-
-    def __repr__(self):
-        for i in r_list:
-            return str(i)
+        self.atick = atick
 
     def roster_entry():
         r_list = []
@@ -40,25 +30,11 @@ class Roster():
                 r_list.append(i)
         return r_list
 
-
-class CallTypes():
-    """
-    call types that go into tickets
-    type: this is where the full type name goes ie "Unit Assist: Patient Issues"
-    priority: 0-5 priority level. 0 = Unavailable, 1 = Stationary, 2 = One high priority call 3 = Break,
-    4 = On low priority call, 5 = Available.
-    backup: how many officers need to get assigned to this call
-    """
-    def __init__(self, type, priority, backup):
-        self.type = type
-        self.priority = priority
-        self.backup = backup
-
-    call_dict = {"UAP": "Unit Assist Patient Issues", "UAV": "Unit Assist Visitor Issues""}
+    def __repr__(self):
+        return("Post:%s Officer:%s Zone:%s Status:%s Ticket:%s" % (self.post, self.uid, self.zone, self.status, self.atick))
 
 
-
-#        post, uid, zone, status, atick
+#         post, uid, zone, status, atick
 p1 = Roster(1, None, None, 0, None)
 p2 = Roster(2, None, 2, 1, None)
 p3 = Roster(3, None, 3, 1, None)
@@ -75,5 +51,28 @@ p12 = Roster(12, None, 12, 5, None)
 source_list = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12]
 
 
-Roster.roster_entry()
+class CallType():
+    """
+    call types that go into tickets
+    ctype: this is where the abbreviated name goes ie "UAV"
+    priority: 0-3 priority level. 0 = Low, 1 = Medium, 2 = High 3 = Emergency
+    backup: how many officers need to get assigned to this call
+    ftype: the full name of the call type from the call_dict dictionary
+    """
+    def __init__(self, ctype, priority, backup, ftype):
+        self.ctype = ctype
+        self.priority = priority
+        self.backup = backup
+        self.ftype = ftype
 
+    def __repr__(self):
+        return"Call Type:%s Priority:%s Backup Needed:%s Full Type:%s" % (self.ctype, self.priority, self.backup,
+                                                                          self.ftype)
+
+
+#              ctype, priority, backup, ftype
+UAP = CallType("UAP", 3, 3, "Unit Assist: Patient Issues")
+UAV = CallType("UAV", 2, 2, "Unit Assist: Visitor Issues")
+ACC = CallType("ACC", 0, 1, "Access Door Request")
+
+call_list = [UAP, UAV, ACC]
